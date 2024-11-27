ae92ed02a3ce689843b846fca962cfbb6517885e
jaluspeet <jaluspit@gmail.com>
2024-11-27 15:48:09 +0100

    makefile -> install.sh

--- Makefile
+++
@@ -1,7 +0,0 @@
-copy:
-	cp -rf ./alacritty ~/.config/
-	cp -rf ./git/.gitconfig ~
-	cp -rf ./i3 ~/.config/
-	cp -rf ./i3status ~/.config/
-	cp -rf ./vim/.vimrc ~
-	cp -rf ./zsh/.zshrc ~
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cp -rf ./alacritty ~/.config/
+cp -rf ./git/.gitconfig ~
+cp -rf ./i3 ~/.config/
+cp -rf ./i3status ~/.config/
+cp -rf ./vim/.vimrc ~
+cp -rf ./zsh/.zshrc ~
+cp -rf .vscode/settings.json ~/.config/Code/User/
+cp -rf .vscode/keybindings.json ~/.config/Code/User/
+cp -rf .vscode/tasks.json ~/.config/Code/User/
+cp -rf .vscode/snippets ~/.config/VSCodium/User/
+cp -rf .vscode/extensions ~/.config/VSCodium/User/
+cp -rf .vscode/settings.json ~/.config/VSCodium/User/
