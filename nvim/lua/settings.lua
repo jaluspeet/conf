cda39341aeee3d02b4d77e180781cfb631f0b428
jaluspeet <jaluspit@gmail.com>
2024-11-29 16:09:45 +0100

    lunaperche

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -14,7 +14,6 @@ vim.o.mouse = 'a'
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.cursorline = true
 vim.wo.wrap = false
 vim.g.filetype = "on"
 vim.o.splitright = true
@@ -29,8 +28,9 @@ vim.o.fillchars='eob: '
 vim.cmd([[aunmenu PopUp.How-to\ disable\ mouse]])
 vim.cmd([[aunmenu PopUp.-1-]])
 
--- vim.cmd.colorscheme 'wildcharm'
+vim.cmd.colorscheme 'lunaperche'
 vim.api.nvim_set_hl(0, 'Normal', { bg='NONE', ctermbg='NONE' })
+vim.api.nvim_set_hl(0, 'Function', { link = 'Changed' })
 vim.api.nvim_create_autocmd('TermOpen', { command = 'setlocal nonumber norelativenumber' })
 vim.api.nvim_create_autocmd('FileType', { pattern = 'qf', command = 'setlocal norelativenumber' })
 vim.api.nvim_create_autocmd('FileType', { pattern = 'dap-repl', command = 'setlocal nonumber norelativenumber | lua require("dap.ext.autocompl").attach()' })
